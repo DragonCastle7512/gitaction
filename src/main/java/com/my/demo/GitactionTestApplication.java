ce5e62fa31f9c9210384cb1d1d219a1ff5bf1b40
김용성 <kimys75128386@gmail.com>
2025-08-04 15:47:22 +0900

    delete target

--- src/main/java/com/my/demo/GitactionTestApplication.java
+++ src/main/java/com/my/demo/GitactionTestApplication.java
@@ -2,11 +2,8 @@ package com.my.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @SpringBootApplication
 @RestController
